4d74910551366a36f92afd7e5450c2fd5845e11a
NxPKG <iconmamundentist@gmail.com>
2024-08-25 01:06:02 +0600

    Initial commit

---
+++ packages/app/src/app/hooks/useBetaSandboxEditor.ts
@@ -0,0 +1,5 @@
+// This will be removed in the first cleanup post release, but for now
+// it is the simplesy way to ensure everyone is on the v2 editor
+export const useBetaSandboxEditor = (): [boolean] => {
+  return [true];
+};
